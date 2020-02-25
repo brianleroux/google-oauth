7939e3c59588b294b2afc64539cef97d45a0d8ae
Brian LeRoux <b@brian.io>
2020-02-25 12:58:51 -0800

    adds sign in link maybe

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,14 +1,23 @@
-let arc = require('@architect/functions')
+const { google } = require('googleapis')
+const { http } = require('@architect/functions')
 
-async function http(req) {
+async function handler(req) {
   if (req.path === '/') {
+    let clientID = process.env.GOOGLE_CLIENT_ID
+    let secret = process.env.GOOGLE_CLIENT_SECRET
+    let redirect = process.env.GOOGLE_REDIRECT_URL
+    let oAuth2Client = new google.auth.OAuth2(clientID, secret, redirect)
+    let url = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/gmail.readonly'
+    })
     return {
-      html: 'good luck'
+      html: `<a href=${url}>Sign in with Google</a>`
     }
   }
   else {
-    return arc.http.proxy.public({spa: false})(req)
+    return http.proxy.public({spa: false})(req)
   }
 }
 
-exports.handler = arc.http.async(http)
+exports.handler = http.async(handler)
